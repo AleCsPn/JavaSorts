83a266a7b994368e4df7e16fb0c3e484b17ad12a
Alexandre <alexandrecostapenhasilva@gmail.com>
2026-08-04 22:46:06 -0300

    Aula 04/08/2026

--- src/App.java
+++ src/App.java
@@ -1,34 +1,61 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
+import java.time.LocalDate;
 
 public class App {
+    private static Integer[] gerarVetorAleatorio(int n) {
+        Random random = new Random();
+        Integer[] vetor = new Integer[n];
 
-    public static Integer[] vetorDeNumeros() {
-        return new Integer[]{
-            87, 14, 63, 29, 95,
-            //41, 72, 8, 56, 33
-        };
+        for (int i = 0; i < n; i++) {
+            vetor[i] = random.nextInt(n * 10);
+        }
+        return vetor;
     }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
+        int n = 200000;
+        Integer[] numeros = gerarVetorAleatorio(n);
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
-        Integer[] numeros = vetorDeNumeros();
-
-        System.out.println("\n=== Vetor não ordenado: ===");
-        System.out.println(Arrays.toString(numeros));
-
-        System.out.println("\n=== Ordenando Vetor: ===");
-        bubbleSort.sortVerbose(numeros); //ordenando
-        System.out.println("\n=== Vetor Ordenado: ===");
-        System.out.println(Arrays.toString(numeros));
-
-        // Ordem decrescente
-        System.out.println("\n=== Oredenando: Ordem decrescente ===");
-        numeros = vetorDeNumeros();
-        bubbleSort.sortVerbose(numeros, Comparator.reverseOrder());
-
-        System.out.println("\n=== Ordem decrescente ===");
-        System.out.println(Arrays.toString(numeros));
+        //System.out.println("Vetor original: "+ Arrays.toString(args));
+        long inicio = System.currentTimeMillis();
+        bubbleSort.sort(numeros);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo = " + (fim - inicio) + " ms");
+        //System.out.println("Vetor organizado: "+ Arrays.toString(args));
 
     }
+
+    /*
+     * / public static Pessoa[] vetorDePessoas() {
+     * return new Pessoa[] {
+     * new Pessoa("João", LocalDate.of(1990, 1, 1)),
+     * new Pessoa("Ana", LocalDate.of(1992, 2, 2)),
+     * new Pessoa("Maria", LocalDate.of(1995, 5, 5)),
+     * new Pessoa("Pedro", LocalDate.of(1993, 4, 4)),
+     * new Pessoa("Lucas", LocalDate.of(1991, 6, 6)),
+     * new Pessoa("Carlos", LocalDate.of(1988, 3, 3)),
+     * new Pessoa("Ana", LocalDate.of(1985, 2, 2))
+     * };
+     * }
+     * 
+     * public static void main(String[] args) {
+     * BubbleSort<Pessoa> bubbleSortPessoas = new BubbleSort<>();
+     * Pessoa[] pessoas = vetorDePessoas();
+     * ComparadorData comparadorData = new ComparadorData();
+     * 
+     * Comparator<Pessoa> comparadorNomeData = (p1, p2) -> {
+     * int compNome = p1.getNome().compareTo(p2.getNome());
+     * if (compNome != 0) {
+     * return compNome;
+     * }
+     * return p1.getDataNascimento().compareTo(p2.getDataNascimento());
+     * };
+     * 
+     * bubbleSortPessoas.sort(pessoas, comparadorNomeData);
+     * for (Pessoa p : pessoas) {
+     * System.out.println(p);
+     * }
+     * }
+     */
 }
